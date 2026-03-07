b439039c71bb0f33a1af2ad88dd896e3341e6b95
Nathanael <Shakadak@users.noreply.github.com>
2026-03-07 19:43:09 +0100

    fira code over the default (jetbrains mono)

--- wezterm.lua
+++ wezterm.lua
@@ -14,10 +14,12 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 10
+config.font_size = 12
 config.color_scheme = 'Solarized Light (Gogh)'
 
 config.default_prog = {'/usr/bin/zsh'}
+
+config.font = wezterm.font('Fira Code')
 
 -- Finally, return the configuration to wezterm:
 return config
